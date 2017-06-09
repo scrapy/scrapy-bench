f6dbaf139863cacf0ed52454091c564b48581448
Parth-vader <vermaparth97@gmail.com>
2017-06-09 12:48:59 +0530

    bookfiles.tar.gz and sites.tar.gz changed to contain only the files. Updated README.md with the number of html files. The time calculation is more accurate now for the micro-benchmarks.

--- xpathbench.py
+++ xpathbench.py
@@ -3,20 +3,24 @@
 import glob
 import io
 from timeit import default_timer as timer
+import tarfile
 
 from scrapy.http import HtmlResponse, TextResponse, Response, request
 import click
 
 
 def main():
-    start = timer()
     total = 0
-
-    for files in glob.glob('bookfile/*'):
-        f = (io.open(files, "r", encoding="utf-8"))
+    time = 0
+    tar = tarfile.open("bookfiles.tar.gz")
+    for member in tar.getmembers():
+        f = tar.extractfile(member)
         html = f.read()
 
         response = HtmlResponse(url="local", body=html, encoding='utf8')
+
+        start = timer()
+
         rating = response.css(
             'p.star-rating::attr(class)').extract_first().split(' ')[-1]
         title = response.css('.product_main h1::text').extract_first()
@@ -26,16 +30,20 @@ def main():
             response.css('.product_main .instock.availability ::text').re('(\d+)'))
         category = ''.join(
             response.css('ul.breadcrumb li:nth-last-child(2) ::text').extract()).strip()
+
+        end = timer()
         page = [rating, title, price, stock, category]
+
         total = total + 1
-    end = timer()
+        time = time + end - start
+
     print("\nTotal number of pages extracted = {0}".format(total))
-    print("Time taken = {0}".format(end - start))
+    print("Time taken = {0}".format(time))
     click.secho("Rate of link extraction : {0} pages/second\n".format(
-        float(total / (end - start))), bold=True)
+        float(total / time)), bold=True)
 
     with open("Benchmark.txt", 'w') as g:
-        g.write(" {0}".format((float(total / (end - start)))))
+        g.write(" {0}".format((float(total / time))))
 
 
 if __name__ == "__main__":
