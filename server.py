5a492bea3c3e9966eeadffd9b116c244a204e916
Parth <vermaparth97@gmail.com>
2017-08-26 23:58:42 +0530

    added support for python3

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 import random
 import re
+import sys
 
+if sys.version_info[0] < 3:
+	from six.moves.urllib.parse import urlsplit
+else:
+	import urllib.parse.urlsplit
 from twisted.web.server import Site, NOT_DONE_YET
 from twisted.web.resource import Resource
 from twisted.internet import reactor, endpoints
-
-from six.moves.urllib.parse import urlsplit
 
 
 class Home(Resource):
