7981249102745e4b48d4c1d126b61a064e6ed578
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-08-27 15:06:18 +0300

    setup.py cleanup: fix indent, console_scripts as dict

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from setuptools import setup
 
+
 setup(
-        name='Scrapy-Benchmark',
-        version='1.0',
-        py_modules=['scrapy-bench'],
-        install_requires=[
-            'Click',
-            'scrapy',
-            'statistics',
-            'six',
-        ],
-        entry_points='''
-		[console_scripts]
-		scrapy-bench=bench:cli
-		''',
+    name='Scrapy-Benchmark',
+    version='1.0',
+    py_modules=['scrapy-bench'],
+    install_requires=[
+        'Click',
+        'scrapy',
+        'statistics',
+        'six',
+    ],
+    entry_points={
+        'console_scripts': [
+            'scrapy-bench=bench:cli',
+        ]
+    }
 )
