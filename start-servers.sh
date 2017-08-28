6806177a6d2b2740bab02933cc4430b1180bfed3
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-08-28 14:54:37 +0300

    A separate start-servers script

---
+++ start-servers.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -ev
+
+# An entry point for the docker container:
+# change /etc/hosts, start nginx and the broad server.
+
+service nginx start
+python3.6 \
+    -c 'print("\n".join(f"127.0.0.1    domain{i}" for i in range(1, 1001)))' \
+    >> /etc/hosts
+python3.6 server.py &
