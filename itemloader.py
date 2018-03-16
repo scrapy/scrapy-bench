df25d3ef319f29fa22277b84fe23c718c876b882
nctl144 <nctl144@gmail.com>
2018-03-16 02:17:31 -0400

    work with the whole zip file

--- itemloader.py
+++ itemloader.py
@@ -23,29 +23,29 @@ def main():
     time = 0
     tar = tarfile.open("bookfiles.tar.gz")
 
-    first_page = tar.getmembers()[0]
-    f = tar.extractfile(first_page)
-    html = f.read()
-    response = HtmlResponse(url="local", body=html, encoding='utf8')
+    for member in tar.getmembers():
+        f = tar.extractfile(member)
+        html = f.read()
+        response = HtmlResponse(url="local", body=html, encoding='utf8')
 
-    for i in xrange(0, 1000):
-        start = timer()
-        loader = ItemLoader(item=ItemloaderItem(), response=response)
-        loader.add_xpath(
-            'rating', '//*[@id="content_inner"]/article/div[1]/div[2]/p[3]/i[1]')
-        loader.add_xpath(
-            'title', '//*[@id=("content_inner")]/article/div[1]/div[2]/h1')
-        loader.add_xpath(
-            'price', '//*[@id=("content_inner")]/article/div[1]/div[2]/p[1]')
-        loader.add_css('stock', '.product_main .instock.availability ::text')
-        loader.add_css('category', 'ul.breadcrumb li:nth-last-child(2) ::text')
-        loader.add_value('name', 'item {}'.format(i))
-        loader.add_value('url', 'http://site.com/item{}'.format(i))
-        product = loader.load_item()
-        end = timer()
+        for i in xrange(0, 10):
+            start = timer()
+            loader = ItemLoader(item=ItemloaderItem(), response=response)
+            loader.add_xpath(
+                'rating', '//*[@id="content_inner"]/article/div[1]/div[2]/p[3]/i[1]')
+            loader.add_xpath(
+                'title', '//*[@id=("content_inner")]/article/div[1]/div[2]/h1')
+            loader.add_xpath(
+                'price', '//*[@id=("content_inner")]/article/div[1]/div[2]/p[1]')
+            loader.add_css('stock', '.product_main .instock.availability ::text')
+            loader.add_css('category', 'ul.breadcrumb li:nth-last-child(2) ::text')
+            loader.add_value('name', 'item {}'.format(i))
+            loader.add_value('url', 'http://site.com/item{}'.format(i))
+            product = loader.load_item()
+            end = timer()
 
-        total += 1
-        time = time + end - start
+            total += 1
+            time = time + end - start
 
 
     print("\nTotal number of items extracted = {0}".format(total))
