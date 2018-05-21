cd36176fe69e8ada5cea4debd667799065e2c425
nctl144 <nctl144@gmail.com>
2018-05-21 15:44:12 -0400

    make a variant of urlparseprofile that uses profilehooks

--- urlparseprofile.py
+++ urlparseprofile.py
@@ -10,56 +10,64 @@ from scrapy.http import HtmlResponse
 
 
 def main():
-    total = 0
-    time = 0
-    time_file_uri_to_path = 0
-    time_safe_url_string = 0
-    time_canonicalize_url = 0
 
-    tar = tarfile.open("sites.tar.gz")
-    urls = []
+    from profilehooks import profile
+    urlparseprofile_name = "urlparseprofile.prof"
 
-    for member in tar.getmembers():
-        f = tar.extractfile(member)
-        html = f.read()
-        response = HtmlResponse(url="local", body=html, encoding='utf8')
+    @profile(filename=urlparseprofile_name)
+    def profilehooks_profling():
+        total = 0
+        time = 0
+        time_file_uri_to_path = 0
+        time_safe_url_string = 0
+        time_canonicalize_url = 0
 
-        links = response.css('a::attr(href)').extract()
-        urls.extend(links)
+        tar = tarfile.open("sites.tar.gz")
+        urls = []
 
-    for url in urls:
-        start_file_uri_to_path = timer()
-        file_uri_to_path(url)
-        end_file_uri_to_path = timer()
-        time_file_uri_to_path += (end_file_uri_to_path - start_file_uri_to_path)
-        time += (end_file_uri_to_path - start_file_uri_to_path)
+        for member in tar.getmembers():
+            f = tar.extractfile(member)
+            html = f.read()
+            response = HtmlResponse(url="local", body=html, encoding='utf8')
 
-        start_safe_url_string = timer()
-        safe_url_string(url)
-        end_safe_url_string = timer()
-        time_safe_url_string += (end_safe_url_string - start_safe_url_string)
-        time += (end_safe_url_string - start_safe_url_string)
+            links = response.css('a::attr(href)').extract()
+            urls.extend(links)
 
-        start_canonicalize_url = timer()
-        canonicalize_url(url)
-        end_canonicalize_url = timer()
-        time_canonicalize_url += (end_canonicalize_url - start_canonicalize_url)
-        time += (end_canonicalize_url - start_canonicalize_url)
+        for url in urls:
+            start_file_uri_to_path = timer()
+            file_uri_to_path(url)
+            end_file_uri_to_path = timer()
+            time_file_uri_to_path += (end_file_uri_to_path - start_file_uri_to_path)
+            time += (end_file_uri_to_path - start_file_uri_to_path)
 
-        # any_to_uri(url) # Error on Python 2: KeyError: u'\u9996'
+            start_safe_url_string = timer()
+            safe_url_string(url)
+            end_safe_url_string = timer()
+            time_safe_url_string += (end_safe_url_string - start_safe_url_string)
+            time += (end_safe_url_string - start_safe_url_string)
 
-        total += 1
+            start_canonicalize_url = timer()
+            canonicalize_url(url)
+            end_canonicalize_url = timer()
+            time_canonicalize_url += (end_canonicalize_url - start_canonicalize_url)
+            time += (end_canonicalize_url - start_canonicalize_url)
 
-    print("\nTotal number of items extracted = {0}".format(total))
-    print("Time spent on file_uri_to_path = {0}".format(time_file_uri_to_path))
-    print("Time spent on safe_url_string = {0}".format(time_safe_url_string))
-    print("Time spent on canonicalize_url = {0}".format(time_canonicalize_url))
-    print("Total time taken = {0}".format(time))
-    click.secho("Rate of link extraction : {0} items/second\n".format(
-        float(total / time)), bold=True)
+            # any_to_uri(url) # Error on Python 2: KeyError: u'\u9996'
 
-    with open("Benchmark.txt", 'w') as g:
-        g.write(" {0}".format((float(total / time))))
+            total += 1
+
+        print("\nTotal number of items extracted = {0}".format(total))
+        print("Time spent on file_uri_to_path = {0}".format(time_file_uri_to_path))
+        print("Time spent on safe_url_string = {0}".format(time_safe_url_string))
+        print("Time spent on canonicalize_url = {0}".format(time_canonicalize_url))
+        print("Total time taken = {0}".format(time))
+        click.secho("Rate of link extraction : {0} items/second\n".format(
+            float(total / time)), bold=True)
+
+        with open("Benchmark.txt", 'w') as g:
+            g.write(" {0}".format((float(total / time))))
+
+    profilehooks_profling()
 
 
 if __name__ == "__main__":
