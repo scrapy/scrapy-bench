5f5a049e3e79e8f3fe5e2d0baa6aa0a8fa27d702
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2020-09-09 14:47:54 +0000

    handle all request sources and payload in meta

--- scrapy_bench/middlewares.py
+++ scrapy_bench/middlewares.py
@@ -1,17 +1,40 @@
 import os
 
+from scrapy import Request
+
 
 class RandomPayloadMiddleware:
     def __init__(self, size):
+        """
+        Add a random payload to test network effects
+
+        :param name: size of payload added in bytes
+        :type name: int
+
+        """
         self.size = size
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(crawler.settings.getint('SCRAPY_BENCH_RANDOM_PAYLOAD_SIZE'))
 
+    def handle_request(self, smth):
+        if not isinstance(smth, Request):
+            return smth
+
+        request = smth
+
+        if self.size == 0:
+            return request
+
+        request.meta['random_payload'] = os.urandom(self.size)
+        return request
+
+
     def process_start_requests(self, start_requests, spider):
         for request in start_requests:
-            if self.size and request.body == b'':
-                yield request.replace(body=os.urandom(self.size))
-            else:
-                yield request
+            yield self.handle_request(request)
+
+    def process_spider_output(self, response, result, spider):
+        for smth in result:
+            yield self.handle_request(smth)
